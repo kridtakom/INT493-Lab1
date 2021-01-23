df1aac7c3d2135b6721a004f8374a822d84a0b38
Kridtakom <kridtakom.boom@mail.kmutt.ac.th>
2021-01-23 22:42:13 +0700

    first commit

---
+++ src/main/java/webapilab/DataModel.java
@@ -0,0 +1,17 @@
+package webapilab;
+
+import java.util.ArrayList;
+
+public class DataModel {
+
+    private ArrayList<TextModel> data = new ArrayList<TextModel>();
+
+    public ArrayList<TextModel> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<TextModel> data) {
+        this.data = data;
+    }
+
+}
---
+++ src/main/java/webapilab/TextModel.java
@@ -0,0 +1,28 @@
+package webapilab;
+
+public class TextModel {
+    private String text;
+    private int count;
+
+    public TextModel(String text, int count) {
+        this.text = text;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
